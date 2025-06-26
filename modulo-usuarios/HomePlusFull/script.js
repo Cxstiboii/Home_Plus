6215efc55439c820a9d7d43cc563e40fa24ab52b
Dxrxd1710 <juandavidtorres1710@gmail.com>
2025-06-25 23:37:53 -0500

    Update script.js

--- modulo-usuarios/HomePlusFull/script.js
+++ modulo-usuarios/HomePlusFull/script.js
@@ -5,32 +5,33 @@ document.getElementById("loginForm").addEventListener("submit", function(e) {
   const password = document.getElementById("password").value.trim();
   const error = document.getElementById("error");
 
-  if (!email || !password || !role) {
-    error.textContent = "Completa todos los campos y selecciona un rol.";
+  if (!email || !password) {
+    error.textContent = "Completa todos los campos.";
     return;
   }
 
+  // Usuarios válidos
   const usuarios = [
-    { email: "cli@homeplus.com", password: "1234", rol: "cliente" },
+    { email: "cliente@homeplus.com", password: "1234", rol: "cliente" },
     { email: "pro@homeplus.com", password: "5678", rol: "profesional" }
   ];
 
   const usuarioValido = usuarios.find(user =>
     user.email === email &&
-    user.password === password &&
-   
+    user.password === password
   );
 
   if (usuarioValido) {
-    error.textContent = "";
-    alert(`¡Inicio exitoso! Usted se registro como ${role}.`);
+    alert(`¡Inicio exitoso! Usted se registró como ${usuarioValido.rol}.`);
 
-    if (role === "cliente") {
-      window.location.href = "/confirmacion y agendamiento/home/html/trabajosaceptados.html";
-    } else if (role === "profesional") {
-      window.location.href = "/Servicios Publicados/serviciosPublicados.html";
+    // Redirección automática según el tipo de usuario
+    if (usuarioValido.rol === "cliente") {
+      window.location.href = "cliente.html";
+    } else if (usuarioValido.rol === "profesional") {
+      window.location.href = "profesional.html";
     }
   } else {
-    error.textContent = "Correo, contrasena o rol incorrecto.";
+    error.textContent = "Correo o contraseña incorrectos.";
   }
 });
+
